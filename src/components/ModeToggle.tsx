61f59b87c9567ccf8e0ed51c6791cfd25f2fec99
desiboli <adam.benals@gmail.com>
2024-11-10 08:24:44 +0100

    mode toggle ui

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,13 +1,5 @@
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import { Moon, Sun, Monitor } from "lucide-react"
 
 export function ModeToggle() {
   const [theme, setThemeState] = React.useState<
@@ -28,25 +20,19 @@ export function ModeToggle() {
   }, [theme])
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setThemeState("theme-light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="rounded-sm overflow-hidden inline-flex bg-gray-100 dark:bg-neutral-900">
+      <button onClick={() => setThemeState("system")} className="p-2">
+        <Monitor className="size-3" />
+        <span className="sr-only">Enable system theme</span>
+      </button>
+      <button onClick={() => setThemeState("dark")} className="p-2">
+        <Moon className="size-3" />
+        <span className="sr-only">Enable dark theme</span>
+      </button>
+      <button onClick={() => setThemeState("theme-light")} className="p-2">
+        <Sun className="size-3" />
+        <span className="sr-only">Enable light theme</span>
+      </button>
+    </div>
   )
 }
