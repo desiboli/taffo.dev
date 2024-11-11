84d23f2efbdca19d00e186ab94bfd6e9b8f14454
desiboli <adam.benals@gmail.com>
2024-11-11 22:22:08 +0100

    site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Mustafa Alshammaa's Blog"
-export const SITE_DESCRIPTION = "Welcome to my website!"
+export const SITE_DESCRIPTION =
+  "Welcome to my space! – I build high quality and impactful digital experiences"
