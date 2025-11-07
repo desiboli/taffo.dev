fade1f64a7a88103580587c115d6c83f54e1fb68
desiboli <adam.benals@gmail.com>
2025-11-07 23:06:08 +0100

    code splitting cleanup

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Mustafa Alshammaa's Portfolio"
+export const SITE_DESCRIPTION =
+  "Welcome to my space! – I build high quality and impactful digital experiences"
