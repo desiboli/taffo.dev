19315b94b4d547905040554a1ccbb9aa1ea1e6db
desiboli <adam.benals@gmail.com>
2025-11-10 21:54:05 +0100

    improve markup

--- src/components/testimonial-carousel.tsx
+++ src/components/testimonial-carousel.tsx
@@ -62,9 +62,9 @@ export function TestimonialCarousel() {
           >
             <div className="p-1">
               <div className="flex flex-col gap-2">
-                <h3 className="font-semibold text-lg leading-relaxed md:text-xl">
+                <h2 className="font-semibold text-lg leading-relaxed md:text-xl">
                   {testimonial.testimonial}
-                </h3>
+                </h2>
                 <p className="text-muted-foreground text-sm">
                   {testimonial.name}
                 </p>
